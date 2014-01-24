0bb1839c4d435a6e45b4bab3afd454c63ae8d7fb
kriordan <keith.riordan@gmail.com>
2014-01-23 23:59:08 -0500

    Add a full_title helper

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -7,10 +7,16 @@ feature "Static pages" do
     expect(page).to have_content('Welcome to TweetTweetHooray')
   end
 
-  scenario "home page should have the right title" do
+  scenario "home page should have the base title" do
     visit '/static_pages/home'
-    expect(page).to have_title("TweetTweetHooray | Home")
+    expect(page).to have_title("TweetTweetHooray")
   end
+
+  scenario "home page should not have a custom page title" do
+    visit '/static_pages/home'
+    expect(page).not_to have_title(" | Home")
+  end
+
 
 
   scenario "help page should have the content 'Do you need assistance?'" do
@@ -24,6 +30,7 @@ feature "Static pages" do
   end
 
 
+
   scenario "about page should have the content 'A little history for you'" do
     visit '/static_pages/about'
     expect(page).to have_content('A little history for you')
@@ -33,6 +40,7 @@ feature "Static pages" do
     visit '/static_pages/about'
     expect(page).to have_title("TweetTweetHooray | About")
   end
+
 
 
   scenario "contact page should have be present with corrent title" do
